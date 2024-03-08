a43533c0391e7fb6888e48fd750b5d02ffbd5dfa
vngonugondla <veenagonugondla@gmail.com>
2024-03-08 18:07:05 -0500

    input meal screen display features

--- app/src/main/java/com/example/greenplate/viewmodels/InputMealViewModel.java
+++ app/src/main/java/com/example/greenplate/viewmodels/InputMealViewModel.java
@@ -1,4 +1,76 @@
 package com.example.greenplate.viewmodels;
+import androidx.lifecycle.ViewModel;
+import java.util.Date;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
-public class InputMealViewModel {
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+public class InputMealViewModel extends ViewModel{
+
+    private String name;
+    private int calories;
+    private Date date;
+
+    public void InputMeal() {
+
+    }
+
+    public void InputMeal(String name, int calories, Date date) {
+        this.name = name;
+        this.calories = calories;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories(DatabaseReference userReference) {
+        if (userReference != null) {
+            int totalCalories = 0;
+            for (Map<String, Object> meal : userReference.get("Meals")) {
+                totalCalories += (int) meal.get("calories");
+            }
+        }
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getUserInfo(DatabaseReference userReference) {
+        if (userReference != null) {
+            int height = userReference.get("Height");
+            int weight = userReference.get("Weight");
+            String gender = userReference.get("Gender");
+            String userInfoText = "Height: " + height + " cm\n" +
+                    "Weight: " + weight + " kg\n" +
+                    "Gender: " + gender);
+            return userInfoText;
+        } else {
+            return "Failed";
+        }
+
+    }
+
+    public int getCalorieGoal() {
+        //need to write formula here
+        return 100;
+    }
 }
--- app/src/main/java/com/example/greenplate/views/InputMealView.java
+++ app/src/main/java/com/example/greenplate/views/InputMealView.java
@@ -9,17 +9,58 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.greenplate.R;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.greenplate.model.User;
+import com.example.greenplate.viewmodels.AccountCreationViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.TextView;
+import com.example.greenplate.viewmodels.InputMealViewModel;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class InputMealView extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private InputMealViewModel viewModel;
+    private TextView userInfoTextView;
+    private TextView calorieGoalTextView;
+    private TextView dailyCalorieIntakeTextView;
+    private DatabaseReference mDatabase;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+        viewModel = new ViewModelProvider(this).get(InputMealViewModel.class);
+        userInfoTextView = findViewById(R.id.userInfoTextView);
+        calorieGoalTextView = findViewById(R.id.calorieGoalTextView);
+        dailyCalorieIntakeTextView = findViewById(R.id.dailyCalorieIntakeTextView);
+        mAuth = FirebaseAuth.getInstance();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        fetchUserData();
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
+
+    private void fetchUserData() {
+        DatabaseReference userReference = mDatabase.child("users").child(username).get();
+
+        String userData = viewModel.getUserInfo(userReference);
+        userInfoTextView.setText(userData);
+        int totalCalories = viewModel.getCalories(userReference);
+        int calorieGoal = viewModel.getCalorieGoal();
+        dailyCalorieIntakeTextView.setText("Daily Calorie Intake: " + totalCalories);
+        calorieGoalTextView.setText("Calorie Goal: " + calorieGoal);
+    }
+
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
