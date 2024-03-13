cb325fcf2657c08241713be71e1f647be7284175
vngonugondla <66545342+vngonugondla@users.noreply.github.com>
2024-03-12 23:50:47 -0400

    Update AccountCreationView.java

--- app/src/main/java/com/example/greenplate/views/AccountCreationView.java
+++ app/src/main/java/com/example/greenplate/views/AccountCreationView.java
@@ -35,7 +35,7 @@ public class AccountCreationView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_create_account); //setting xml file
 
         viewModel = new ViewModelProvider(this).get(AccountCreationViewModel.class);
 
